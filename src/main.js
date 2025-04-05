906989ed7ce0c61ca03fe1b2ca33750c5e719f18
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-04-05 22:46:30 +0300

    main

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 import './js/hero.js'
 import './js/MyLibrary.js'
 import './js/MovieList.js'
+
+
+import './js/catalog.js'
+import './js/header.js'
+import './js/modal.js'
+import './js/weekly_trends.js'
