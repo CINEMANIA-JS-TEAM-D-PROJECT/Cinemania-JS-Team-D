b96b8b2640f9176292fd81126cf4159e21936918
OzgenGuler <oguler93@gmail.com>
2025-04-06 23:33:18 +0300

    a

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
-import './js/hero.js'
-import './js/MyLibrary.js'
-import './js/MovieList.js'
-
-
-import './js/catalog.js'
-import './js/header.js'
-import './js/modal.js'
-import './js/weekly_trends.js'
+import "./js/hero.js";
+import "./js/MyLibrary.js";
+import "./js/MovieList.js";
+import "./js/catalog.js";
+import "./js/header.js";
+import "./js/modal.js";
+import "./js/weekly_trends.js";
