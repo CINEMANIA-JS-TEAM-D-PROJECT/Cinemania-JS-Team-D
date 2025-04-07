dbf1aadb710d1c2f4d55161124f86157f25a0184
Mevlüde Özdemir <mevlde.ozdemir@gmail.com>
2025-04-08 00:03:30 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './js/header.js';
 import './js/hero.js';
 import './js/weekly_trends.js';
-// import './js/upcoming.js';
+import './js/upcoming.js';
 import './js/footer.js';
 // import './js/MyLibrary.js';
 // import './js/catalog.js';
